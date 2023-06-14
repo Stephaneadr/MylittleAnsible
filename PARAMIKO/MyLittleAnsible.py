d30a30bfeb7b1053eb5fc35c10ea9f86a5ff751a
haggag_e <haggag_e@etna-alternance.net>
2023-06-14 22:35:23 +0200

    ajout fichier test et fonction copy

--- PARAMIKO/MyLittleAnsible.py
+++ PARAMIKO/MyLittleAnsible.py
@@ -9,6 +9,9 @@ import subprocess
 from paramiko import SSHClient
 from typing import Dict
 
+source_dir = '/chemin/du/dossier/source'
+destination_dir = '/chemin/du/dossier/destination'
+
 class CmdResult:
     def __init__(self, stdout: str, stderr: str, exit_code: int):
         self.stdout = stdout
@@ -21,6 +24,14 @@ def connect_to_host(hostname, username):
     client.load_system_host_keys()
     client.connect(hostname, username=username)
     return client
+
+def copy(hostname, username, source_dir, destination_dir):
+    client = connect_to_host(hostname, username)
+
+    scp = client.open_sft()
+    scp.put(source_dir, destination_dir, recursive=True)
+    scp.close()
+    client.close()
 
 
 def apt_package_management(package_name, desired_state, ssh_client):
@@ -71,6 +82,8 @@ def run_remote_cmd(command: str, ssh_client: SSHClient) -> CmdResult:
     except Exception as e:
         result.stderr = str(e)
     return result
+
+
 
 
 def execute_playbook(playbook_file, inventory_file):
